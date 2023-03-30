33192ccda8adc0242b9a9e2081bcf30ffce3d9e9
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2023-03-30 18:17:14 +0300

    fix modal and add style

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,8 +2,6 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
-const modalRoot = document.getElementById('modal-root');
-
 export class Modal extends Component {
   static propTypes = {
     children: PropTypes.node,
